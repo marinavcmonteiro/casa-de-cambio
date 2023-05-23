34ae02a79188b9722d9e1286183360515f2f4ea9
Mariana Ramiro <marianaramiroof@gmail.com>
2023-05-23 17:11:24 -0300

    'Requisito 3'

--- src/main.js
+++ src/main.js
@@ -1,1 +1,19 @@
 import './style.css';
+
+const buttonSubmit = document.querySelector('.buttonSubmit');
+const inputElement = document.querySelector('#input');
+
+buttonSubmit.addEventListener('click', (event) => {
+    event.preventDefault();
+
+    const MOEDA_API = `https://api.exchangerate.host/latest?base=${inputElement.value}`;
+    
+    fetch(MOEDA_API)
+        .then((response) => response.json())
+        .then((data) => console.log(data))
+
+})
+
+
+
+    
